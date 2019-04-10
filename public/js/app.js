4079bf6463d39e7d12264f05ca050963a96336bb
Pavan Kumar Polavarapu <Pavan Kumar>
2019-04-10 11:36:57 -0400

    Webspeech only runs on chrome

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ $(function(){
 const socket = io();
 const query = document.querySelector('#comment');
 const html = document.querySelector('#conversation');
-
+var is_chrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
 
 document.querySelector('#chat-send').addEventListener('click', () => {
 	var date = new Date();
@@ -49,36 +49,37 @@ function replyMain(e){
     	console.log(query.value);
     	}
 };
+if(is_chrome)
+{
+	const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+	const oSpeechRecognition = new SpeechRecognition();
+	oSpeechRecognition.lang = 'en-US';
+	oSpeechRecognition.interimResults = false;
+	oSpeechRecognition.maxAlternatives = 1;
 
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const oSpeechRecognition = new SpeechRecognition();
-oSpeechRecognition.lang = 'en-US';
-oSpeechRecognition.interimResults = false;
-oSpeechRecognition.maxAlternatives = 1;
+	document.querySelector('#microphone-send').addEventListener('click', () => {
+		oSpeechRecognition.start();
+	});
 
-document.querySelector('#microphone-send').addEventListener('click', () => {
-	oSpeechRecognition.start();
-});
+	oSpeechRecognition.addEventListener('result', (e) => {
+		console.log('Finalized the Result.');
 
-oSpeechRecognition.addEventListener('result', (e) => {
-	console.log('Finalized the Result.');
+		let previous = e.results.length - 1;
+		let text = e.results[previous][0].transcript;
 
-	let previous = e.results.length - 1;
-	let text = e.results[previous][0].transcript;
+		query.textContent = text;
+		console.log('Confidence: ' + e.results[0][0].confidence);
+		console.log(e);
+	});
 
-	query.textContent = text;
-	console.log('Confidence: ' + e.results[0][0].confidence);
-	console.log(e);
-});
+	oSpeechRecognition.addEventListener('speechend', () => {
+		oSpeechRecognition.stop();
+	});
 
-oSpeechRecognition.addEventListener('speechend', () => {
-	oSpeechRecognition.stop();
-});
-
-oSpeechRecognition.addEventListener('error', (e) => {
-	outputBot.textContent = 'Error: ' + e.error;
-});
-
+	oSpeechRecognition.addEventListener('error', (e) => {
+		outputBot.textContent = 'Error: ' + e.error;
+	});
+}
 function speak(text) {
 	const speechSynthesis = window.speechSynthesis;
 	const oSpeechSynthesisUtternace = new SpeechSynthesisUtterance();
@@ -87,7 +88,10 @@ function speak(text) {
 }
 
 socket.on('ai response', function(response) {
-	speak(response);
+	if(is_chrome)
+	{
+		speak(response);
+	}
 	var date = new Date();
 	if(response == '') response = '(No answer...)';
 	var htmlResponse = "<div class=\"row message-body\">\
